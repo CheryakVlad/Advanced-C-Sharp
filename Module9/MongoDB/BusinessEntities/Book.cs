94966c156d3de29f59db1e062fa472f8ed63fb74
CheryakVlad <33634585+CheryakVlad@users.noreply.github.com>
2018-12-03 09:39:52 +0300

    Added Module 9 - Introduction to NoSql

---
+++ Module9/MongoDB/BusinessEntities/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDB.BusinessEntities
+{
+	public class Book
+	{
+		public ObjectId Id
+		{
+			get; set;
+		}
+
+		[BsonElement("name")]
+		public string Name
+		{
+			get; set;
+		}
+
+		[BsonElement("author")]
+		public string Author
+		{
+			get; set;
+		}
+
+		[BsonElement("count")]
+		public int Count
+		{
+			get; set;
+		}
+
+		[BsonElement("genre")]
+		public string[] Genre
+		{
+			get; set;
+		}
+
+		[BsonElement("year")]
+		public int Year
+		{
+			get; set;
+		}
+	}
+}

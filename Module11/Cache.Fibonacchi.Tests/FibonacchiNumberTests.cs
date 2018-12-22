2c5abe43f473d9688309411f850735d18f27a491
CheryakVlad <33634585+CheryakVlad@users.noreply.github.com>
2018-12-22 14:44:50 +0300

    Refactoring Module 11 - added region

--- Module11/Cache.Fibonacchi.Tests/FibonacchiNumberTests.cs
+++ Module11/Cache.Fibonacchi.Tests/FibonacchiNumberTests.cs
@@ -13,53 +13,101 @@ namespace Cache.Fibonacchi.Tests
 		[Fact]
 		public void Test_should_check_Fibonacci_MemoryCache()
 		{
+			#region ARRANGE
+
 			Cache<long> cache = new Cache<long>("fibonacchi");
 			FibonacchiNumber fibonacchi = new FibonacchiNumber(cache);
 
+			#endregion
+
+			#region ACT
+
 			long result = fibonacchi.Calculate(5);
+
+			#endregion
+
+			#region ASSERT
 
 			Assert.Equal(5, result);
 			Assert.Equal(5, cache.Get("5"));
+
+			#endregion
 		}
 
 		[Fact]
 		public void Test_should_check_expiration_date_Fibonacci_MemoryCache()
 		{
+			#region ARRANGE
+
 			Cache<long> cache = new Cache<long>("fibonacchi");
 			FibonacchiNumber fibonacchi = new FibonacchiNumber(cache);
 
+			#endregion
+
+			#region ACT
+
 			long result = fibonacchi.Calculate(5);
+
+			#endregion
+
+			#region ASSERT
 
 			Assert.Equal(5, result);
 			Assert.Equal(5, cache.Get("5"));
 			Thread.Sleep(2001);
 			Assert.NotEqual(5, cache.Get("5"));
+
+			#endregion
 		}
 
 		[Fact]
 		public void Test_should_check_Fibonacci_RedisCache()
 		{
+			#region ARRANGE
+
 			RedisCache<long> redisCache = new RedisCache<long>("localhost", "fibonacchi");
 			FibonacchiNumber fibonacchi = new FibonacchiNumber(redisCache);
 
+			#endregion
+
+			#region ACT
+
 			long result = fibonacchi.Calculate(5);
+
+			#endregion
+
+			#region ASSERT
 
 			Assert.Equal(5, result);
 			Assert.Equal(5, redisCache.Get("5"));
+
+			#endregion
 		}
 
 		[Fact]
 		public void Test_should_check_expiration_date_Fibonacci_RedisCache()
 		{
+			#region ARRANGE
+
 			RedisCache<long> redisCache = new RedisCache<long>("localhost", "fibonacchi");
 			FibonacchiNumber fibonacchi = new FibonacchiNumber(redisCache);
 
+			#endregion
+
+			#region ACT
+
 			long result = fibonacchi.Calculate(5);
+
+			#endregion
+
+			#region ASSERT
 
 			Assert.Equal(5, result);
 			Assert.Equal(5, redisCache.Get("5"));
 			Thread.Sleep(2001);
 			Assert.NotEqual(5, redisCache.Get("5"));
+
+			#endregion
 		}
 
 		#endregion
